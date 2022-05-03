aae366b036b594fe0403405602335b322b2d5f99
gtn1024 <tony.bazhang@gmail.com>
2022-05-03 12:42:46 +0000

    Update at 2022-05-03T12:42:46.293279Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,8 +1,10 @@
 package com.bytelegend;
 
 public interface Challenge {
-    /** Gets the number of seconds from the Java epoch of 1970-01-01T00:00:00Z. */
-    long currentTimeSecond() {
+    /**
+     * Gets the number of seconds from the Java epoch of 1970-01-01T00:00:00Z.
+     */
+    static long currentTimeSecond() {
         return System.currentTimeMillis() / 1000;
     }
 
