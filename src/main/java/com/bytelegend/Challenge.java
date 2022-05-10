7743edd1be675f2880c348a7e056e98baf235832
xxldm <zenglengceng@outlook.com>
2022-05-10 12:19:37 +0000

    Update at 2022-05-10T12:19:37.522413Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public interface Challenge {
     /** Gets the number of seconds from the Java epoch of 1970-01-01T00:00:00Z. */
-    long currentTimeSecond() {
+    static long currentTimeSecond() {
         return System.currentTimeMillis() / 1000;
     }
 
